ab9bf996a5b2f4d8e8c116486d8dd7533082a410
meddle <meddlesmerch@gmail.com>
2015-01-05 11:49:54 +0200

    New ProAct.js YUIDoc documentation.

---
+++ doc/api.js
@@ -0,0 +1,28 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "ProAct",
+        "ProAct.Actor",
+        "ProAct.ActorUtil",
+        "ProAct.Configuration",
+        "ProAct.Observable",
+        "ProAct.States",
+        "ProAct.Utils"
+    ],
+    "modules": [
+        "proact",
+        "proact-core"
+    ],
+    "allModules": [
+        {
+            "displayName": "proact",
+            "name": "proact"
+        },
+        {
+            "displayName": "proact-core",
+            "name": "proact-core",
+            "description": "TODO Move it to its own file."
+        }
+    ]
+} };
+});
